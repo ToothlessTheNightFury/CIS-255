1be2e309d193ff8cc0d83986bb09db9ddcd1dab8
Benson Yee <bensonyee128@gmail.com>
2021-01-23 05:51:29 -0800

    Removed stringTokenize from comment

--- Project 1/Project 1/src/Utils.java
+++ Project 1/Project 1/src/Utils.java
@@ -41,7 +41,6 @@ import java.util.StringTokenizer;
  *  - promptNumRange
  *  - promptStr
  *  - promptYN
- *  - stringTokenize
  *
  * This work is licensed under the Creative Commons Attribution 4.0
  * International License. To view a copy of this license, visit
