b5f13b00145692beb06a00356e958fb540c0b887
Benson Yee <bensonyee128@gmail.com>
2021-01-23 07:30:55 -0800

    Add files via upload

---
+++ Project 1 (Updated)/CreditCardVerification.java
@@ -0,0 +1,22 @@
+public class CreditCardVerification {
+
+    public static void main (String[] args) {
+
+        Utils.printTitle("Credit Card Verification", "Benson Yee");
+
+        CreditCard card = new CreditCard();
+        card = CreditCard.promptCreditCard();
+        card.printFullInfo();
+
+        Utils.printVar("cardNumber", card.getCardNumber());
+        Utils.printVar("getCardNumberNum()", card.getCardNumberNum());
+        System.out.println();
+
+        Utils.printVar("cardExpirationMonth", card.getCardExpirationMonth());
+        Utils.printVar("cardExpirationYear", card.getCardExpirationYear());
+        Utils.printVar("getCardExpirationDate()", card.getCardExpirationDate());
+        System.out.println();
+
+        Utils.printCredits("Benson Yee");
+    }
+}
