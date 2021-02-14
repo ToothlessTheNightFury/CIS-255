d70e6f9786237ac849aa8d8717e6da614c12ddf0
Benson Yee <bensonyee128@gmail.com>
2021-02-14 05:15:11 -0800

    Add files via upload

---
+++ Practice Questions/m1_q8/m1_q8.java
@@ -0,0 +1,17 @@
+/*
+public class m1_q8 {
+
+    public static void printPizzaPerPerson (int numberOfPizzas, int numberOfPeople) {
+
+        double pizzasPerPerson = (double) numberOfPizzas / (double) numberOfPeople;
+        System.out.printf("Each person gets %s pizzas.\n", String.valueOf(pizzasPerPerson).replaceFirst("\\.0*$|(\\.\\d*?)0+$", "$1"));
+    }
+
+    public static void main (String[] args) {
+
+        printPizzaPerPerson(3, 5);
+        printPizzaPerPerson(4, 3);
+        printPizzaPerPerson(4, 2);
+    }
+}
+*/
