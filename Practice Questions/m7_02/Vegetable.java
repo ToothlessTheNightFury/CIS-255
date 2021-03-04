cb0bb769dead10be16ddf396026307bf7371b512
Benson Yee <bensonyee128@gmail.com>
2021-03-03 22:52:48 -0800

    Update Vegetable.java

--- Practice Questions/m7_02/Vegetable.java
+++ Practice Questions/m7_02/Vegetable.java
@@ -1,47 +1,47 @@
 public class Vegetable extends Food {
 
-    private final boolean DEFAULT_IS_GREEN = true;
-    private boolean isGreen;
+	private final boolean DEFAULT_IS_GREEN = true;
+	private boolean isGreen;
 
-    public Vegetable (String name) {
-    	this(name, DEFAULT_IS_GREEN);
-    }
+	public Vegetable (String name) {
+		this(name, DEFAULT_IS_GREEN);
+	}
 
-    public Vegetable (String name, boolean isGreen) {
-        this.name = name;
-        this.isGreen = isGreen;
-    }
+	public Vegetable (String name, boolean isGreen) {
+	    this.name = name;
+	    this.isGreen = isGreen;
+	}
 
-    public String getName() {
-    	return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName (String name) {
-    	this.name = name;
-    }
+	public void setName (String name) {
+		this.name = name;
+	}
 
-    public boolean getIsGreen() {
-    	return isGreen;
-    }
+	public boolean getIsGreen() {
+		return isGreen;
+	}
 
-    public void setIsGreen (boolean isGreen) {
-    	this.isGreen = isGreen;
-    }
+	public void setIsGreen (boolean isGreen) {
+		this.isGreen = isGreen;
+	}
 
-    @Override
-    public String toString() {
-    	return String.format("%s (Vegetable%s)", getName(), getIsGreen() ? ", Green" : "");
-    }
+	@Override
+	public String toString() {
+		return String.format("%s (Vegetable%s)", getName(), getIsGreen() ? ", Green" : "");
+	}
 
-    @Override
-    public boolean equals (Object obj) {
-    	
-    	if (obj instanceof Vegetable) {
+	@Override
+	public boolean equals (Object obj) {
+		
+		if (obj instanceof Vegetable) {
 
-    		Vegetable other = (Vegetable) obj;
-    		return getName().equals(other.getName()) && getIsGreen() == other.getIsGreen(); 
-    	}
+			Vegetable other = (Vegetable) obj;
+			return getName().equals(other.getName()) && getIsGreen() == other.getIsGreen(); 
+		}
 
-    	return false;
-    }
+		return false;
+	}
 }
